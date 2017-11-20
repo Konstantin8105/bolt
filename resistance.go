08d854471814f105b744f4ca127771d504e79337
Konstantin8105 <konstantin8105@gmail.com>
2017-11-20 20:58:01 +0300

    separate files

---
+++ resistance.go
@@ -0,0 +1,27 @@
+package bolt
+
+/*
+	private double EN1993_1_8_TABLE_3_4_FtRd(double Pub, double As, double gamma_M2)
+	{
+	    double k2 = 0.9;
+	    return k2 * Pub * As / gamma_M2;
+	}
+
+	private double EN1993_1_8_TABLE_3_4_FvRd(double Pub, double As, double gamma_M2, BOLT_CLASS _BS)
+	{
+	    double alphaV = 0.0;
+	    switch(_BS)
+	    {
+	        case g4_6 : alphaV = 0.6; break;
+	        case g4_8 : alphaV = 0.5; break;
+	        case g5_6 : alphaV = 0.6; break;
+	        case g5_8 : alphaV = 0.5; break;
+	        case g6_8 : alphaV = 0.5; break;
+	        case g8_8 : alphaV = 0.6; break;
+	        case g10_9: alphaV = 0.5; break;
+	        default:
+	        	alphaV = 0.0;
+	    }
+	    return alphaV * Pub * As / gamma_M2;
+	}
+*/
